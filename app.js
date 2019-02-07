87173af356d58ad5cc84b8a00784fab863bf3b56
Shappy <HapShapIncorporated@gmail.com>
2019-02-07 15:01:55 -0500

    Half-Validation

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+var methodOverride = require("method-override");
 var port = 5000;
 var path = require('path');
+var session = require('express-session');
+var passport = require('passport'); //passport-local
+var flash = require('connect-flash');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var {ensureAuthenticated} = require('./helpers/auth');
+
+//Configures routes
+var users = require('./routes/users');
+
+//Passportjs Config route
+require('./config/passport')(passport);
 
 //gets ride of warning for Mongoose
 mongoose.Promise = global.Promise;
@@ -29,8 +40,31 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Setup Express Session
+app.use(session({
+    secret:"Secret",
+    resave:true,
+    saveUninitialized:true
+}));
+
+//Setup Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//confiure flash messages
+/*app.use(flash());
+app.use(function(req, res){
+    res.locals.success.mesg = req.flash('success');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});*/
+
+app.use(methodOverride('_method'));
+
 //Route to index
-router.get('/', function(req, res){
+router.get('/', ensureAuthenticated, function(req, res){
     //res.sendFile(path.join(__dirname+'/index.html'));
     //var title = "Welcome to the GameApp page";
 
@@ -51,7 +85,7 @@ router.get('/gameentries/edit/:id', function(req, res){
     
 });
 //Route to put edited entry
-router.post('/editgame/:id', function(req,res){
+router.put('/editgame/:id', function(req,res){
     Entry.findOne({
         _id:req.params.id
     }).then(function(entry){
@@ -69,7 +103,14 @@ router.get('/login', function(req, res){
     res.render('login');
 });
 
-app.get('/', function(req, res){
+router.post('/login', function(req, res, next){
+    passport.authenticate('local', {
+        successRedirect:'/',
+        failureRedirect:'/login'
+    })(req, res, next);
+});
+
+app.get('/', ensureAuthenticated, function(req, res){
     //console.log("Request made from fetch");
     Entry.find({}).then(function(entries){
         res.render("index", {
@@ -96,7 +137,7 @@ app.post('/addgame', function(req, res){
 });
 
 //Delete Game Entry
-app.post('/:id', function(req,res){
+app.delete('/:id', function(req,res){
     Entry.remove({_id:req.params.id})
     .then(function(){
         //req.flash("Game Removed");
@@ -108,7 +149,8 @@ app.post('/:id', function(req,res){
 app.use(express.static(__dirname+'/views'));
 app.use(express.static(__dirname+'/scripts'))
 app.use('/', router);
-
+app.use('/users', users);
+ 
 //starts server
 app.listen(port, function(){
     console.log('Server is running on Port: ' + port);
